8c5d301770a55135723d85e2a4629a04159a6e9d
JustSonaram <102964581+JustSonaram@users.noreply.github.com>
2022-04-16 20:30:14 +0700

    Update user_name.py

--- metods/user_name.py
+++ metods/user_name.py
@@ -3,4 +3,7 @@ import json
 player_name = input("Player Name: ")
 url = requests.get("https://api.vimeworld.ru/user/name/" + player_name)
 player = url.json()[0]
+"""Using The print() command, they showed: Username, Level, Rank."""
 print("Name: " + player["username"] + "\nLevel: " + str(player["level"]) + "\nRank: " + player["rank"])
+"""Und Save Player ID"""
+player_id = player["id"]
